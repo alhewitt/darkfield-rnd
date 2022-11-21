0b4f91885c4ec84250b21b95fe2b3cb5a65f5374
alhewitt <a.hewitt@lancaster.ac.uk>
2022-11-21 11:13:30 +0000

    Update sounds-test.py

--- sounds-test.py
+++ sounds-test.py
@@ -1,49 +1,26 @@
 import pygame
 from pygame.locals import *
 import os
-import time
 
-os.chdir('C:/Users/amyhe/OneDrive/Documents/Visual Studio 2019/')
+# Set working directory
+os.chdir('C:/Users/amyhe/OneDrive/Documents/GitHub/pygame-stuff/')
 
-pygame.init()
-width = 1000
-height = 500
-window = pygame.display.set_mode((width,height))
-pygame.mixer.init()
-pygame.display.set_mode() 
+pygame.init()             # Initialise pygame  
+pygame.mixer.init()       # Initialise mixer
+pygame.display.set_mode() # Initialise display window   
 
+# Import sounds
 song1 = pygame.mixer.Sound('sounds/song.wav')
 song2 = pygame.mixer.Sound('sounds/song2.wav')
 ding = pygame.mixer.Sound('sounds/ding.wav')
 
+pygame.mixer.Channel(0).play(song1) # Play song on code run
 
-# while True:
-#     pygame.mixer.Channel(0).play(song1)
-#     # pygame.mixer.Channel(1).play(song2)
-#     # time.sleep(5)
-#     for event in pygame.event.get():
-#         if event.type == QUIT:
-#             pygame.quit()
-#             break
-#         if event.type == KEYDOWN:
-#             pygame.mixer.Channel(1).play(song2)
-#             time.sleep(5)
-
-# ding = pygame.mixer.Sound(dingfile)
-
-pygame.mixer.Channel(0).play(song1)
-
-while True:
-    # time.sleep(10)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit(); #sys.exit() if sys is imported
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_m:
-                pygame.mixer.Channel(1).play(ding)
-        pygame.display.update()
-
-
-
-
-# pygame.mixer.Channel(2).play(ding)
+while True: # Infinite loop
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: # Enable quitting
+            pygame.quit(); 
+        if event.type == pygame.KEYDOWN:           # If a key is pressed
+            if event.key == pygame.K_m:            # and the key is m,
+                pygame.mixer.Channel(1).play(ding) # then play ding sound
+        pygame.display.update()                    # and update display
