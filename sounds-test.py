1a0056c10234abf8406b0e31f99ed4ce88d8b37b
alhewitt <a.hewitt@lancaster.ac.uk>
2022-11-21 11:36:20 +0000

    Update sounds-test.py

--- sounds-test.py
+++ sounds-test.py
@@ -9,7 +9,7 @@ pygame.init()             # Initialise pygame
 pygame.mixer.init()       # Initialise mixer
 pygame.display.set_mode() # Initialise display window   
 
-# Import sounds
+# Load sounds
 song1 = pygame.mixer.Sound('sounds/song.wav')
 song2 = pygame.mixer.Sound('sounds/song2.wav')
 ding = pygame.mixer.Sound('sounds/ding.wav')
@@ -17,10 +17,10 @@ ding = pygame.mixer.Sound('sounds/ding.wav')
 pygame.mixer.Channel(0).play(song1) # Play song on code run
 
 while True: # Infinite loop
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: # Enable quitting
-            pygame.quit(); 
+    for event in pygame.event.get():  # For any event (i.e. key press)
+        if event.type == pygame.QUIT: # if event is a quit event, (i.e. alt-F4)
+            pygame.quit()             # quit game
         if event.type == pygame.KEYDOWN:           # If a key is pressed
-            if event.key == pygame.K_m:            # and the key is m,
+            if event.key == pygame.K_d:            # and the key is d,
                 pygame.mixer.Channel(1).play(ding) # then play ding sound
         pygame.display.update()                    # and update display
